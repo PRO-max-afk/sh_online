afcbc663dba94c585c16061fe078bbffc5fbc7c6
PRO <sarwarimostafa8@gamil.com>
2025-04-24 11:27:14 +0430

    calenadar

---
+++ GUI/calendars.py
@@ -0,0 +1,164 @@
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QGridLayout, QLabel, QComboBox, QPushButton
+from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt, QPropertyAnimation, QEasingCurve, QTimer
+import jdatetime
+
+class JalaliCalendar(QDialog):
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        self.main_window = main_window
+        self.setWindowFlags(Qt.WindowType.Popup)
+        self.setWindowOpacity(0)  # شروع با شفافیت صفر برای انیمیشن
+        self.setFixedSize(310, 230)
+        self.setStyleSheet("""
+            QDialog {
+                background-color: white;
+                border: 1px solid #ccc;
+                border-radius: 10px;
+            }
+        """)
+
+        self.font = QFont("B Nazanin", 12)
+        self.setFont(self.font)
+
+        self.current_date = jdatetime.date.today()
+        self.current_year = self.current_date.year
+        self.current_month = self.current_date.month
+
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(10, 10, 10, 10)
+        self.layout.setSpacing(10)
+
+        self.header_layout = QHBoxLayout()
+
+        self.year_label = QLabel(str(self.current_year))
+        self.year_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.year_label.setFont(QFont("B Nazanin", 14, QFont.Weight.Bold))
+
+        self.month_combo = QComboBox()
+        self.month_names = [
+            "", "حمل", "ثور", "جوزا", "سرطان", "اسد", "سنبله",
+            "میزان", "عقرب", "قوس", "جدی", "دلو", "حوت"
+        ]
+        self.month_combo.addItems(self.month_names[1:])
+        self.month_combo.setCurrentIndex(self.current_month - 1)
+        self.month_combo.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+        self.month_combo.setStyleSheet('''
+            QComboBox {
+                background-color: white;
+                font-family: "B Nazanin";
+                font-size: 12px;
+                font-weight: bold;
+                color: #000;
+                border: 1px solid #bfbfbf;
+                border-radius: 8px;
+                text-align: right;
+                padding: 6px 10px 6px 30px;
+            }
+            QComboBox::drop-down {
+                subcontrol-origin: padding;
+                subcontrol-position: top left;
+                width: 30px;
+                border: none;
+            }
+            QComboBox QAbstractItemView {
+                background-color: white;
+                color: black;
+                text-align: left;
+                font-family: "B Nazanin";
+                font-size: 12px;
+                border: 1px solid #bfbfbf;
+                border-radius: 8px;
+                selection-background-color: #f0f0f0;
+            }
+            QComboBox::down-arrow {
+                image: url(assets/Down Button.png);
+                width: 20px;
+                height: 20px;
+            }
+        ''')
+        self.month_combo.currentIndexChanged.connect(self.on_month_changed)
+
+        self.header_layout.addWidget(self.month_combo)
+        self.header_layout.addStretch()
+        self.header_layout.addWidget(self.year_label)
+        self.layout.addLayout(self.header_layout)
+
+        self.days_layout = QGridLayout()
+        self.days_layout.setHorizontalSpacing(12)
+        self.days_layout.setVerticalSpacing(10)
+        self.layout.addLayout(self.days_layout)
+
+        self.update_calendar()
+
+    def show_with_animation(self, pos):
+        self.move(pos)
+        self.setWindowOpacity(0)
+        self.show()
+        self.fade_anim = QPropertyAnimation(self, b"windowOpacity")
+        self.fade_anim.setDuration(300)
+        self.fade_anim.setStartValue(0)
+        self.fade_anim.setEndValue(1)
+        self.fade_anim.setEasingCurve(QEasingCurve.Type.OutCubic)
+        self.fade_anim.start()
+
+    def hide_with_animation(self):
+        self.fade_anim = QPropertyAnimation(self, b"windowOpacity")
+        self.fade_anim.setDuration(300)
+        self.fade_anim.setStartValue(1)
+        self.fade_anim.setEndValue(0)
+        self.fade_anim.setEasingCurve(QEasingCurve.Type.OutCubic)
+        self.fade_anim.finished.connect(self.close)
+        self.fade_anim.start()
+
+    def on_month_changed(self, index):
+        self.current_month = index + 1
+        self.update_calendar()
+
+    def update_calendar(self):
+        self.year_label.setText(str(self.current_year))
+
+        for i in reversed(range(self.days_layout.count())):
+            widget = self.days_layout.itemAt(i).widget()
+            if widget:
+                widget.setParent(None)
+
+        first_day = jdatetime.date(self.current_year, self.current_month, 1)
+        start_col = (first_day.togregorian().weekday() + 1) % 7
+
+        day = 1
+        row = 0
+        for i in range(start_col, 42):
+            try:
+                jdatetime.date(self.current_year, self.current_month, day)
+            except:
+                break
+
+            btn = QPushButton(str(day))
+            btn.setFont(QFont("B Nazanin", 11))
+            btn.setFixedSize(40, 40)
+            btn.setStyleSheet("""
+                QPushButton {
+                    background-color: transparent;
+                    color: black;
+                    border: 1px solid transparent;
+                    border-radius: 20px;
+                }
+                QPushButton:hover {
+                    background-color: #f5f5f5;
+                }
+                QPushButton:pressed {
+                    background-color: #498bf5;
+                }
+            """)
+            btn.clicked.connect(lambda checked, d=day: self.select_day(d))
+            self.days_layout.addWidget(btn, row, i % 7)
+            day += 1
+            if (i + 1) % 7 == 0:
+                row += 1
+
+    def select_day(self, day):
+        selected = jdatetime.date(self.current_year, self.current_month, day)
+        formatted = selected.strftime("%Y/%m/%d")
+        self.main_window.set_selected_date(formatted)
+        self.hide_with_animation()
--- GUI/new_p.py
+++ GUI/new_p.py
@@ -1,12 +1,14 @@
 from PyQt6.QtWidgets import (
-    QApplication, QWidget, QVBoxLayout, QLabel, QPushButton,QFrame,QGraphicsDropShadowEffect,QComboBox,
-    QLineEdit, QFileDialog, QHBoxLayout, QDialog)
-from PyQt6.QtGui import QPixmap, QFont,QColor
+    QApplication, QWidget, QVBoxLayout, QLabel, QPushButton,QFrame,QGraphicsDropShadowEffect,QComboBox,QGridLayout,QFileDialog,
+    QLineEdit, QFileDialog, QHBoxLayout, QDialog,QWidget)
+from PyQt6.QtGui import QPixmap, QFont,QColor,QIcon
 import sys
 import jdatetime
 from profile_picture import ProfileImage
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt,QPropertyAnimation,QEasingCurve
+from PyQt6 import QtCore
 import os
+from calendars import JalaliCalendar
 
 class ProductForm(QDialog):
     def __init__(self):
@@ -31,24 +33,56 @@ class ProductForm(QDialog):
         ##
         self.cate_lb= QLabel("دسته بندی محصول:", self)
         self.choise_c= QComboBox(self)
-       
+        ##
+        self.under_ct= QLabel("نوعیت محصول:",self)
+        self.under_choise= QComboBox(self)
+       ##
+        self.name_lb= QLabel("نام محصول:", self)
+        self.name_line= QLineEdit(self)
         ##
         self.bar_lb= QLabel("بارکد محصول:",self)
         self.bar_line= QLineEdit(self)
         ##
-        self.name_lb= QLabel("نام محصول:", self)
-        self.name_line= QLineEdit(self)
-        ##
         self.exp_name= QLabel("تاریخ انقضاء:", self)
         self.exp_line= QLineEdit(self)
+        self.exp_line.setReadOnly(True)
+        ##
+        self.layout = QVBoxLayout(self)
+        self.layout.setSpacing(15)
+        self.layout.setContentsMargins(20, 20, 20, 20)
+        self.jalali_calendar = JalaliCalendar(self)
+        self.jalali_calendar.setMaximumHeight(0)  # در ابتدا بسته باشد
+        self.layout.addWidget(self.jalali_calendar)
         ##
         self.category= QLabel("کتگوری عمده:",self)
         self.cate_ch= QComboBox(self)
+        
         ##
-
+        self.buy_price= QLabel("قیمت خرید:",self)
+        self.buy_line= QLineEdit(self)
+        ##
+        self.number_lb= QLabel("تعداد محصول:", self)
+        self.number_line= QLineEdit(self)
+        ##
+        self.sale_price= QLabel("قیمت فروش:", self)
+        self.sale_line= QLineEdit(self)
+        ##
+        self.sale_big= QLabel("قیمت عمده:",self)
+        self.sale_big_line= QLineEdit(self)
+        ##
+        self.img_preveiw= QLabel(self)
+        ##
+        self.picture_btn= QPushButton(self)
+        self.submit_btn= QPushButton(self)
+        self.calendar_btn= QPushButton(self)
+        ##
+        self.name_category= ["انتخاب","مواد غذایی","نوشیدنی","لوازم خانه گی و آشپزخانه","لوازم برقی و الکترونیکی","لوازم کودک و اسباب بازی"]
+        self.under_cate= []
         ##
         self.lable_UI()
         self.enties_UI()
+        self.Button_UI()
+        self.under_category()
 
     def center_window(self):
         """مرکز کردن پنجره روی صفحه"""
@@ -76,7 +110,7 @@ class ProductForm(QDialog):
             color: black;
         ''')
         ##
-        self.cate_lb.setGeometry(760,115,150,20)
+        self.cate_lb.setGeometry(745,115,150,20)
         self.cate_lb.setStyleSheet('''
             font-family: B Nazanin;
             font-size: 16px;
@@ -84,7 +118,15 @@ class ProductForm(QDialog):
             color: black;
         ''')
         ##
-        self.bar_lb.setGeometry(480,215,115,20)
+        self.under_ct.setGeometry(450,115,153,20)
+        self.under_ct.setStyleSheet('''
+            font-family: B Nazanin;
+            font-size: 16px;
+            font-weight: bold;
+            color: black;
+        ''')
+        ##
+        self.bar_lb.setGeometry(480,200,115,20)
         self.bar_lb.setStyleSheet('''
             font-family: B Nazanin;
             font-size: 16px;
@@ -92,7 +134,7 @@ class ProductForm(QDialog):
             color: black;
         ''')
         ##
-        self.name_lb.setGeometry(776,215,115,20)
+        self.name_lb.setGeometry(776,200,115,20)
         self.name_lb.setStyleSheet('''
             font-family: B Nazanin;
             font-size: 16px;
@@ -100,7 +142,7 @@ class ProductForm(QDialog):
             color: black;
         ''')
         ##
-        self.exp_name.setGeometry(776,315,120,20)
+        self.exp_name.setGeometry(776,300,120,20)
         self.exp_name.setStyleSheet('''
             font-family: B Nazanin;
             font-size: 16px;
@@ -108,31 +150,72 @@ class ProductForm(QDialog):
             color: black;
         ''')
         ##
-        self.category.setGeometry(480,315,120,20)
+        self.category.setGeometry(480,300,120,20)
         self.category.setStyleSheet('''
             font-family: B Nazanin;
             font-size: 16px;
             font-weight: bold;
             color: black;
         ''')
+        ##
+        self.buy_price.setGeometry(776,400,120,20)
+        self.buy_price.setStyleSheet('''
+            font-family: B Nazanin;
+            font-size: 16px;
+            font-weight: bold;
+            color: black;
+        ''')
+        ##
+        self.number_lb.setGeometry(480,400,120,20)
+        self.number_lb.setStyleSheet('''
+            font-family: B Nazanin;
+            font-size: 16px;
+            font-weight: bold;
+            color: black;
+        ''')
+        ##
+        self.sale_price.setGeometry(776,495,120,20)
+        self.sale_price.setStyleSheet('''
+            font-family: B Nazanin;
+            font-size: 16px;
+            font-weight: bold;
+            color: black;
+            ''')
+        ##
+        self.sale_big.setGeometry(480,495,120,20)
+        self.sale_big.setStyleSheet('''
+            font-family: B Nazanin;
+            font-size: 16px;
+            font-weight: bold;
+            color: black;
+            ''')
+        ##
+        self.img_preveiw.setGeometry(20,191,221,238)
+        self.img_preveiw.setScaledContents(True)
+        self.img_preveiw.setStyleSheet('''
+        background-color: transparent;
+        border: 1px solid #1646B5;
+        ''')
+        ##
     ##
     def enties_UI(self):
-        self.choise_c.setGeometry(355, 150, 545, 45)
-        self.choise_c.addItems(["مواد غذایی","سبزیجات","غله جات"])
-        self.choise_c.setCurrentText("مواد غذایی")
+        self.choise_c.setGeometry(653, 150, 250, 45)
+        self.choise_c.addItems(self.name_category)
+        self.choise_c.setCurrentText(self.name_category[0])
+        self.choise_c.currentTextChanged.connect(self.under_category)
         self.choise_c.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
         self.choise_c.setStyleSheet('''
             QComboBox {
                 background-color: white;
                 font-family: "B Nazanin";
-                font-size: 16px;
+                font-size: 15px;
                 font-weight: bold;
                 color: #000;
                 border: 1px solid #bfbfbf;
                 border-radius: 8px;
                 text-align: right;
                 padding: 6px 10px 6px 30px; /* فضای کافی برای فلش در سمت چپ */
-                padding-left: 440px;
+                padding-left: 77px;
             }
 
             QComboBox::drop-down {
@@ -144,8 +227,8 @@ class ProductForm(QDialog):
 
             QComboBox::down-arrow {
                 image: url(assets/Down Button.png);
-                width: 30px;
-                height: 30px;
+                width: 20px;
+                height: 20px;
             }
 
             QComboBox QAbstractItemView {
@@ -160,7 +243,49 @@ class ProductForm(QDialog):
             }
         ''')
         ##
-        self.name_line.setGeometry(653,250,250,45)
+        self.under_choise.setGeometry(355,150,250,45)
+        self.under_choise.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+        self.under_choise.setStyleSheet('''
+            QComboBox {
+                background-color: white;
+                font-family: "B Nazanin";
+                font-size: 15px;
+                font-weight: bold;
+                color: #000;
+                border: 1px solid #bfbfbf;
+                border-radius: 8px;
+                text-align: right;
+                padding: 6px 10px 6px 30px; /* فضای کافی برای فلش در سمت چپ */
+                padding-left: 100px;
+            }
+
+            QComboBox::drop-down {
+                subcontrol-origin: padding;
+                subcontrol-position: top left; /* انتقال فلش به چپ */
+                width: 30px;
+                border: none;
+            }
+
+            QComboBox::down-arrow {
+                image: url(assets/Down Button.png);
+                width: 20px;
+                height: 20px;
+            }
+
+            QComboBox QAbstractItemView {
+                background-color: white;  /* پس‌زمینه سفید */
+                color: black;             /* متن سیاه */
+                text-align: left;        /* تراز متن به راست */
+                font-family: "B Nazanin";
+                font-size: 15px;
+                border: 1px solid #bfbfbf;
+                border-radius: 8px;
+                selection-background-color: #f0f0f0;  /* رنگ انتخاب آیتم */
+            }
+        ''')
+
+        ##
+        self.name_line.setGeometry(653,230,250,45)
         self.name_line.setStyleSheet('''
             background-color: white;
             font-family: B Nazanin;
@@ -172,7 +297,7 @@ class ProductForm(QDialog):
             padding: 7px;
         ''')
         ##
-        self.bar_line.setGeometry(355,250,250,45)
+        self.bar_line.setGeometry(355,230,250,45)
         self.bar_line.setStyleSheet('''
             background-color: white;
             font-family: Arial;
@@ -184,7 +309,7 @@ class ProductForm(QDialog):
             padding: 7px;
         ''')
         ##
-        self.exp_line.setGeometry(653,350,250,45)
+        self.exp_line.setGeometry(653,330,250,45)
         self.exp_line.setStyleSheet('''
             background-color: white;
             font-family: B Nazanin,"Mirza";
@@ -196,9 +321,9 @@ class ProductForm(QDialog):
             padding: 7px;
         ''')
         ##
-        self.cate_ch.setGeometry(355,350,250,45)
-        self.cate_ch.addItems(["عدد","دانه","بسته"])
-        self.cate_ch.setCurrentText("دانه")
+        self.cate_ch.setGeometry(355,330,250,45)
+        self.cate_ch.addItems(["انتخاب","دانه","کارتن","بسته","کیسه","شانه","جعبه"])
+        self.cate_ch.setCurrentText("انتخاب")
         self.cate_ch.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
         self.cate_ch.setStyleSheet('''
             QComboBox {
@@ -211,7 +336,7 @@ class ProductForm(QDialog):
                 border-radius: 8px;
                 text-align: right;
                 padding: 6px 10px 6px 30px; /* فضای کافی برای فلش در سمت چپ */
-                padding-left: 180px;
+                padding-left: 170px;
             }
 
             QComboBox::drop-down {
@@ -223,8 +348,8 @@ class ProductForm(QDialog):
 
             QComboBox::down-arrow {
                 image: url(assets/Down Button.png);
-                width: 30px;
-                height: 30px;
+                width: 25px;
+                height: 25px;
             }
 
             QComboBox QAbstractItemView {
@@ -239,7 +364,128 @@ class ProductForm(QDialog):
             }
         ''')
         ##
+        self.buy_line.setGeometry(653,430,250,45)
+        self.buy_line.setStyleSheet('''
+            background-color: white;
+            font-family: B Nazanin,"Mirza";
+            font-weight: bold;
+            font-size: 15px;
+            color: black;
+            border: 1px solid #c2c2c2;
+            border-radius: 7px;
+            padding: 7px;
+            ''')
+        ##
+        self.number_line.setGeometry(355,430,250,45)
+        self.number_line.setStyleSheet('''
+            background-color: white;
+            font-family: B Nazanin,"Mirza";
+            font-weight: bold;
+            font-size: 15px;
+            color: black;
+            border: 1px solid #c2c2c2;
+            border-radius: 7px;
+            padding: 7px;
+        ''')
+        ##
+        self.sale_line.setGeometry(653,520,250,45)
+        self.sale_line.setStyleSheet('''
+            background-color: white;
+            font-family: B Nazanin,"Mirza";
+            font-weight: bold;
+            font-size: 15px;
+            color: black;
+            border: 1px solid #c2c2c2;
+            border-radius: 7px;
+            padding: 7px;
+        ''')
+        ##
+        self.sale_big_line.setGeometry(355,520,250,45)
+        self.sale_big_line.setStyleSheet('''
+            background-color: white;
+            font-family: B Nazanin,"Mirza";
+            font-weight: bold;
+            font-size: 15px;
+            color: black;
+            border: 1px solid #c2c2c2;
+            border-radius: 7px;
+            padding: 7px;
+        ''')
 
+    ##
+    def Button_UI(self):
+        self.submit_btn.setGeometry(445,571,358,45)
+        self.sub_icon= QIcon(self.get_asset_path("Bookmark.png"))
+        self.submit_btn.setIcon(self.sub_icon)
+        self.submit_btn.setIconSize(QtCore.QSize(36,36))
+        self.submit_btn.setText("ثبت محصول")
+        self.submit_btn.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+        self.submit_btn.setStyleSheet('''
+            QPushButton {
+                background-color: #3EB516;
+                font-family: "Mirza";
+                font-size: 18px;
+                font-weight: bold;
+                border-radius: 10px;
+                text-align: center;
+                padding: 5px;
+                padding-right: 120px; /* فاصله‌ی داخلی سمت راست */
+                padding-left: 125px;  /* فاصله‌ی داخلی سمت چپ */
+                qproperty-iconSize: 32px;
+
+            }
+            QPushButton:hover {
+                background-color: #84E963;  
+            }
+            QPushButton:pressed {
+                background-color: #3EB516;
+            }
+        ''')
+        ##
+        self.picture_btn.setGeometry(40,134,155,43)
+        self.picture_icon= QIcon(self.get_asset_path("Camera.png"))
+        self.picture_btn.setIcon(self.picture_icon)
+        self.picture_btn.setIconSize(QtCore.QSize(35,35))
+        self.picture_btn.setText("  انتخاب عکس")
+        self.picture_btn.clicked.connect(self.select_image)
+        self.picture_btn.setStyleSheet('''
+             QPushButton {
+                background-color: #2251DB;
+                font-family: "Mirza";
+                font-size: 18px;
+                font-weight: bold;
+                border-radius: 10px;
+                text-align: center;
+                padding: 5px;
+                padding-bottom: 10px;
+            }
+            QPushButton:hover {
+                background-color: #498bf5;  
+            }
+            QPushButton:pressed {
+                background-color: #2251DB;
+            }
+        ''')
+        ##
+        self.calendar_btn.setGeometry(870,337,30,30)
+        self.cale_icon= QIcon(self.get_asset_path("calendar_8265298.png"))
+        self.calendar_btn.setIcon(self.cale_icon)
+        self.calendar_btn.setIconSize(QtCore.QSize(30,30))
+        self.calendar_btn.setStyleSheet('''
+            QPushButton {
+                background-color: white;
+                border: 1px solid transparent;
+                padding: 10px;
+                border-radius: 12px; /* گردی برای همه حالت‌ها */
+            }
+            QPushButton:hover {
+                background-color: #f5f5f5;
+            }
+            QPushButton:pressed {
+                background-color: #d0d0d0;  /* خاکستری ملایم هنگام کلیک */
+            }
+        ''')
+        self.calendar_btn.clicked.connect(self.show_calendar)
     ##
     def set_today_date(self):
         today_jalali = jdatetime.date.today().strftime("%Y/%m/%d")
@@ -251,7 +497,38 @@ class ProductForm(QDialog):
         self.line.setFrameShape(QFrame.Shape.HLine)
         self.line.setFrameShadow(QFrame.Shadow.Sunken)
         self.line.setStyleSheet("color: white; background-color: white;")
+    ##
+    def under_category(self):
+        selected = self.choise_c.currentText()
+        self.under_choise.clear()
 
+        if selected == "مواد غذایی":
+            self.under_cate = ["انتخاب", "خوارکی ها", "سبزیجات و میوه ها", "گوشت و ماهی", "روغن و چربی ها","خشکبار و مغزیجات","حبوبات","لبنیات"]
+        elif selected == "نوشیدنی":
+            self.under_cate = ["انتخاب","چای","قهوه","نوشیدنی انرژی زا","نوشابه و آبمیوه ها"]
+        elif selected == "لوازم خانه گی و آشپزخانه":
+            self.under_cate = ["انتخاب","ظرف", "قاشق و چنگال", "دستمال", "سرویس آشپزخانه"]
+        elif selected == "لوازم برقی و الکترونیکی":
+            self.under_cate = ["انتخاب","تلویزیون", "یخچال", "ماشین لباسشویی", "پنکه"]
+        elif selected == "لوازم کودک و اسباب بازی":
+            self.under_cate = ["انتخاب","عروسک", "ماشین بازی", "لگو", "توپ"]
+        elif selected== "انتخاب":
+            self.under_cate= ["انتخاب"]
+        else:
+            self.under_cate = []
+
+        self.under_choise.addItems(self.under_cate)
+    ##
+    def show_calendar(self):
+        self.calendar_popup = JalaliCalendar(self)
+        pos = self.calendar_btn.mapToGlobal(self.calendar_btn.rect().bottomRight())
+        self.calendar_popup.show_with_animation(pos)
+
+
+
+    ##
+    def set_selected_date(self, date_str):
+        self.exp_line.setText(date_str)
     ##images
     def get_asset_path(self, filename):
         project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -261,7 +538,12 @@ class ProductForm(QDialog):
         else:
             print(f"⚠ فایل یافت نشد: {image_path}")
             return None
-
+    ##
+    def select_image(self):
+        file_path, _= QFileDialog.getOpenFileName(self,"انتخاب تصویر محصول","", "images(*.png *.jpg *.jpeg)")
+        if file_path:
+            self.img_preveiw.setPixmap(QPixmap(file_path))
+            self.img_path= file_path
 
 
 if __name__ == "__main__":
